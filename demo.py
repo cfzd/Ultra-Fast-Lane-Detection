2046b151db625c924bd3851303416fa66b3dbc64
zequnqin <wwwcfzd@yeah.net>
2020-06-28 10:41:47 +0800

    add video demo script

---
+++ demo.py
@@ -0,0 +1,77 @@
+import torch, os, cv2
+from model.model import parsingNet
+from utils.common import merge_config
+from utils.dist_utils import dist_print
+import torch
+import scipy, tqdm
+import numpy as np
+import torchvision.transforms as transforms
+from data.dataset import LaneTestDataset
+
+if __name__ == "__main__":
+    torch.backends.cudnn.benchmark = True
+
+    args, cfg = merge_config()
+
+    dist_print('start testing...')
+    assert cfg.backbone in ['18','34','50','101','152','50next','101next','50wide','101wide']
+
+    if cfg.dataset == 'CULane':
+        cls_num_per_lane = 18
+    elif cfg.dataset == 'Tusimple':
+        cls_num_per_lane = 56
+    else:
+        raise NotImplementedError
+
+    net = parsingNet(pretrained = False, backbone=cfg.backbone,cls_dim = (cfg.griding_num+1,cls_num_per_lane,4),
+                    use_aux=False).cuda() # we dont need auxiliary segmentation in testing
+
+    state_dict = torch.load(cfg.test_model, map_location = 'cpu')
+
+    net.load_state_dict(state_dict['model'])
+    net.eval()
+
+    img_transforms = transforms.Compose([
+        transforms.Resize((288, 800)),
+        transforms.ToTensor(),
+        transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+    ])
+    splits = ['test0_normal.txt', 'test1_crowd.txt', 'test2_hlight.txt', 'test3_shadow.txt', 'test4_noline.txt', 'test5_arrow.txt', 'test6_curve.txt', 'test7_cross.txt', 'test8_night.txt']
+    datasets = [LaneTestDataset(cfg.data_root,os.path.join(cfg.data_root, 'list/test_split/'+split),img_transform = img_transforms) for split in splits]
+
+    for split, dataset in zip(splits, datasets):
+        loader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle = False, num_workers=1)
+        fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+        print(split[:-3]+'avi')
+        vout = cv2.VideoWriter(split[:-3]+'avi', fourcc , 30.0, (1640,590))
+        for i, data in enumerate(tqdm.tqdm(loader)):
+            imgs, names = data
+            imgs = imgs.cuda()
+            with torch.no_grad():
+                out = net(imgs)
+
+            col_sample = np.linspace(0, 800 - 1, cfg.griding_num)
+            col_sample_w = col_sample[1] - col_sample[0]
+
+
+            out_j = out[0].data.cpu().numpy()
+            out_j = out_j[:, ::-1, :]
+            prob = scipy.special.softmax(out_j[:-1, :, :], axis=0)
+            idx = np.arange(cfg.griding_num) + 1
+            idx = idx.reshape(-1, 1, 1)
+            loc = np.sum(prob * idx, axis=0)
+            out_j = np.argmax(out_j, axis=0)
+            loc[out_j == cfg.griding_num] = 0
+            out_j = loc
+
+            # import pdb; pdb.set_trace()
+            vis = cv2.imread(os.path.join(cfg.data_root,names[0]))
+            for i in range(out_j.shape[1]):
+                if np.sum(out_j[:, i] != 0) > 2:
+                    for k in range(out_j.shape[0]):
+                        if out_j[k, i] > 0:
+                            ppp = (int(out_j[k, i] * col_sample_w * 1640 / 800) - 1, int(590 - k * 20) - 1)
+                            cv2.circle(vis,ppp,5,(0,255,0),-1)
+            vout.write(vis)
+        
+        vout.release()
